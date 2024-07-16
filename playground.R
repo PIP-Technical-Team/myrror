34d1ba89cf8a9841d4cbc3c5752d6ff8a8fc44c3
giorgiacek <giorgivola@gmail.com>
2024-07-16 14:23:23 -0400

    clean-up

--- playground.R
+++ playground.R
@@ -1,145 +1,68 @@
-
-# Example of specific task function
-compare_type <- function(dfx = NULL,
-                         dfy = NULL,
-                         constructor = NULL,
-                         output     = getOption("myrror.user.return")) {
-
-  # check that dfx and dfy are not null or that constructor is not null
-  if (is.null(dfx) | is.null(dfy)) {
-    stop("dfx and dfy must be provided")
-  }
-
-  if (is.null(constructor)) {
-    constructor <- myrror_constructor(dfx, dfy)
-  }
-
-  # Type comnparison
-  # Here you do the heavy lifting of the comparison
-
-
-  # Return
-  if (output = "user") {
-    return(x)
+compare_type_int <- function(x, y) {
+  if (is.integer(x) && is.integer(y)) {
+    return(TRUE)
   } else {
-    return(y)
+    return(FALSE)
+  }
+}
+
+
+compare_type <- function(x       = NULL,
+                         y       = NULL,
+                         mo      = NULL,
+                         verbose = TRUE,
+                         output  = c("myrror_object", "simple")) {
+  if (!is.null(mo)) {
+    mo <- create_myrror_object(x,y)
   }
 
+  mo <- compare_type_int(mo) # mo$compare_type
+
+  if (verbose) {
+    mo$compare_type
+    mo$print$compare_type <- TRUE
+    print(mo)
+  }
+
+  if (output == "myrror_object") {
+    return(invisible(mo))
+  } else if (output == "simple") {
+    return(invisible(mo$compare_type))
+  }
 
 }
 
 
-# workhorse function
-myrror <- function(dfx,
-                   dfy,
-                   by = NULL,
-                   by.x = NULL,
-                   by.y = NULL,
-                   #tolerance = NULL, POSTPONED
-                   factor_to_char = TRUE,
-                   compare_type = TRUE,
-                   compare_obs = TRUE
-) {
-
-
-  # 0. Store original datasets and orginal dataset characteristics ----
-  original_call <- match.call()
-  dfx_name <- deparse(substitute(dfx))
-  dfy_name <- deparse(substitute(dfy))
-  original_dfx <- dfx
-  original_dfy <- dfy
-
-  constructor <- myrror_constructor(dfx, dfy, by, by.x, by.y)
-
-  if (compare_type) {
-    ct <- compare_type(constructor = constructor,
-                       output = "myrror")
-  } else {
-    ct <- list()
+myrror <- function(x,y) {
+  if (!is.null(mo)) {
+    mo <- create_myrror_object(x,y)
   }
-
+  if (compare_type = TRUE) {
+    mo <- compare_type_int(mo)
+  }
+  if (compare_values = TRUE) {
+    mo <- compare_values_int(mo)
+  }
 
 }
 
-# actual constructor function
-# whatever is necessary for any comparison
-
-myrror_constructor <- function() {
-  dfx <- check_df(dfx)
-  dfy <- check_df(dfy)
-
-  set_by <- check_set_by(by, by.x, by.y)
 
 
-  dfx_char <- list(
-    nrow = nrow(original_dfx),
-    ncol = ncol(original_dfx)
-  )
-
-  dfy_char <- list(
-    nrow = nrow(original_dfy),
-    ncol = ncol(original_dfy)
-  )
-
-
-
-  sorting_dfx <- is_dataframe_sorted_by(original_dfx, set_by$by.x)
-  sorting_dfy <- is_dataframe_sorted_by(original_dfy, set_by$by.y)
-
-
-  common_vars <- intersect(sorting_dfx[[2]], sorting_dfy[[2]])
-  is_common_sorted = !length(common_vars) == 0
-
-  ## Store
-  datasets_report <- list()
-  datasets_report$dfx_char <- dfx_char
-  datasets_report$dfy_char <- dfy_char
-  datasets_report$sorting_dfx <- sorting_dfx
-  datasets_report$sorting_dfy <- sorting_dfy
-  datasets_report$is_common_sorted <- is_common_sorted
-
-
-  # 5. Prepare Dataset for Join ----
-  # - make into data.table.
-  # - make into valid column names.
-  # - check that by variable are in the colnames of the given dataset.
-  # - factor to character (keep track of this), default = TRUE.
-
-  prepared_dfx <- prepare_df(dfx,
-                             by = set_by$by.x,
-                             factor_to_char = factor_to_char)
-
-  prepared_dfy <- prepare_df(dfy,
-                             by = set_by$by.y,
-                             factor_to_char = factor_to_char)
-
-  # - Check that set_by$by.x is not in the non-key columns of dfy and vice-versa.
-  # Note: this step needs to be done here because the column names might
-  # change in the prepare_df() function.
-  if (any(set_by$by.x %in% setdiff(names(prepared_dfy), set_by$by.y))) {
-    stop("by.x is part of the non-index columns of dfy.")
+print.myrror <- function(x) {
+  if (x$print$compare_type) {
+    cat("Type comparison: ", x$compare_type, "\n")
   }
-  if (any(set_by$by.y %in% setdiff(names(prepared_dfx), set_by$by.x))) {
-    stop("by.y is part of the non-index columns of dfx.")
+  if (x$print$compare_values) {
+    cat("Value comparison: ", x$compare_values, "\n")
   }
-
-  # MERGED DATA REPORT ----
-  # 5. Merge ----
-  # - use collapse to merge and keep matching and non-matching observations.
-
-  ## Merge using Join
-  merged_data <- collapse::join(prepared_dfx,
-                                prepared_dfy,
-                                on=stats::setNames(set_by$by.x, set_by$by.y),
-                                how='full',
-                                sort = TRUE, # now we sort the data by the key
-                                multiple = TRUE,
-                                suffix = c(".x",".y"),
-                                keep.col.order = FALSE,
-                                verbose = 0,
-                                column = list("join", c("x", "y", "x_y")),
-                                attr = TRUE)
-
-  merged_data
 }
 
+
+print.myrror <- function(x) {
+  if (x$print$compare_type) {
+    cat("Type comparison: ", x$compare_type, "\n")
+  }
+  if (x$print$compare_values) {
+    cat("Value comparison: ", x$compare_values, "\n")
+  }
+}
