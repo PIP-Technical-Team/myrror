141a04645090f7e2ceea5cff89b8713c65f7e846
giorgiacek <giorgivola@gmail.com>
2024-07-24 10:06:47 -0400

    check() passed, documentation finished

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,4 @@
 # option 1 (then you should also put utils in Imports)
 utils::globalVariables(c("variable", "column_x", "column_y", "class_x", "class_y",
                          "same_class", ".joyn", "count", "rn", "rn.x", "rn.y",
-                         "row_index.x", "row_index.y", "rwo_index", "indexes"))
+                         "row_index.x", "row_index.y", "row_index", "indexes"))
