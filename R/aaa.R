8db122dbe46d53cf158049a3dbf2af3e3ade933d
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-08-09 15:25:12 -0400

    update global variables

--- R/aaa.R
+++ R/aaa.R
@@ -1,7 +1,3 @@
-# Global Variables ----
-utils::globalVariables(c("variable", "column_x", "column_y", "class_x", "class_y",
-                         "same_class", ".joyn", "count", "rn", "rn.x", "rn.y",
-                         "row_index.x", "row_index.y", "row_index", "indexes", "equal"))
 
 # Package specific environment ----
 .myrror_env <- new.env(parent = emptyenv())
--- R/myrror-package.R
+++ R/myrror-package.R
@@ -1,6 +1,38 @@
+#' @keywords internal
+"_PACKAGE"
+
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
 #' @rawNamespace import(collapse, except = fdroplevels)
 #' @rawNamespace import(data.table, except = fdroplevels)
 ## usethis namespace: end
 .datatable.aware = TRUE
+
+
+# Global Variables ----
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    names = c(
+      "variable",
+      "column_x",
+      "column_y",
+      "class_x",
+      "class_y",
+      "same_class",
+      ".joyn",
+      "count",
+      "rn",
+      "rn.x",
+      "rn.y",
+      "row_index.x",
+      "row_index.y",
+      "row_index",
+      "indexes",
+      "equal"
+    ),
+    package = utils::packageName()
+  )
+
+
+}
+NULL
